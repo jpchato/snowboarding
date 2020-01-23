a0b9b671375f3240ac5ada241bd75215b0a40a13
Jesse Pena <jpchato@gmail.com>
2020-01-22 19:28:52 -0800

    string variable draft

---
+++ string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = '';
+message = '';
+
+var elName = document.getElementById ();
+elName.textContent = username;
+var elNote = document.getElementById ();
+elNote.textContent = message;
